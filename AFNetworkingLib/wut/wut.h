62d4911d77f3aa97237edec6a37300607af38777
Kra Larivain <olarivain@gmail.com>
2015-07-18 12:55:20 -0700

    I think this'll work

---
+++ AFNetworkingLib/wut/wut.h
@@ -0,0 +1,19 @@
+//
+//  wut.h
+//  wut
+//
+//  Created by Olivier Larivain on 7/18/15.
+//  Copyright © 2015 Alamofire. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for wut.
+FOUNDATION_EXPORT double wutVersionNumber;
+
+//! Project version string for wut.
+FOUNDATION_EXPORT const unsigned char wutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wut/PublicHeader.h>
+
+
